5a7917d2c6d35ce7357841be14fa465dcd647fbe
aravizo <digitalsoulai1@gmail.com>
2025-11-29 01:46:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false   // ← السطر ده هو اللي هيوقف الـ terser نهائي
+  }
 })
